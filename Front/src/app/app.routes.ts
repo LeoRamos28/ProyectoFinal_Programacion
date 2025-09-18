35e17258c09bb298a213b2d9a1ffff4a97f02541
Miguel Angel Silva <silvamiguel25582@gmail.com>
2025-09-17 21:15:52 -0300

    Restablecer

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login.component/login.component';
+import { Landing } from './componentes/landing/landing';
 
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },   // <- agregada
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },   // <- agregada
   { path: 'login', component: LoginComponent },
   // opcionalmente, ruta comodín para 404
-  { path: '**', redirectTo: 'landing' }
+  { path: 'landing', component: Landing }
 ];
 
