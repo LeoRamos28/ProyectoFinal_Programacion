bb93e1c03746bc00208df389af6dba36b9d87d5e
Miguel Angel Silva <silvamiguel25582@gmail.com>
2025-11-20 16:38:12 -0300

    Creacion de un listado y filtro de clientes

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -2,22 +2,24 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login.component/login.component';
 import { Landing } from './componentes/landing/landing';
 import { DashboardComponent } from './componentes/dashboard/dashboard';
-import { TecnicoComponent } from './componentes/tecnico/tecnico'; 
+import { TecnicoComponent } from './componentes/tecnico/tecnico';
 import { AtencionClienteComponent } from './componentes/atencion/atencion';
 import { OrdenesTecnicoComponent } from './componentes/ordenes-tecnico/ordenes-tecnico';
+import { Clientes } from './componentes/clientes/clientes';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'landing', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'landing', component: Landing },
-  
+
   {
     path: 'dashboard',
-    component: DashboardComponent, 
+    component: DashboardComponent,
     children: [
-      { path: 'tecnicos', component: TecnicoComponent }, 
-      { path: 'atencion', component: AtencionClienteComponent }, 
-      { path: 'ordenes-tecnico', component: OrdenesTecnicoComponent }, 
+      { path: 'tecnicos', component: TecnicoComponent },
+      { path: 'atencion', component: AtencionClienteComponent },
+      { path: 'ordenes-tecnico', component: OrdenesTecnicoComponent },
+      { path: 'clientes', component: Clientes },
 
       { path: '', redirectTo: 'tecnicos', pathMatch: 'full' }
     ]
---
+++ Front/src/app/services/clientes.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service'; // importa tu AuthService
+
+@Injectable({ providedIn: 'root' })
+export class ClientesService {
+  private apiUrl = 'http://localhost:3000/api/clientes';
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  // Método auxiliar para obtener headers con el token
+  private getAuthHeaders(): HttpHeaders {
+    const token = this.authService.getToken();
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+  }
+
+  getClientes(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl, { headers: this.getAuthHeaders() });
+  }
+
+  buscarClientes(valor: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/buscar?query=${valor}`, { headers: this.getAuthHeaders() });
+  }
+
+
+  updateCliente(id: number, datos: any): Observable<any> {
+  return this.http.put<any>(`${this.apiUrl}/${id}`, datos, { headers: this.getAuthHeaders() });
+}
+deleteCliente(id: number): Observable<any> {
+  return this.http.delete<any>(`${this.apiUrl}/${id}`, { headers: this.getAuthHeaders() });
+}
+}
